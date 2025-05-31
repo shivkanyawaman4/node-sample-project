cbb9a9c83aaf66c318a7256cc929c375c8690ca7
shivkanyawaman4 <wamanshivkanya@gmail.com>
2025-05-31 16:08:38 +0530

    :blossom:

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ const app = express(); //store liabrary for use in file
 port = process.env.PORT || 3000;
 
 const mongoDBConnection = require('./src/config/mongoDbConfiguration')  // import form mongoDBconfiguration file
-console.log("abc");
+// console.log("abc");
 // app.get('/bill', (req, res) => {
 //     res.send("Hello Shivkanya.......");
 // })
 
 //function for listen all requests from frontend or running app on perticular port using listen
 mongoDBConnection();
+// console.log("xyz");
+
 app.listen(port, () => {
     console.log("App is running on port", port)
 });
--- src/config/mongoDbConfiguration.js
+++ src/config/mongoDbConfiguration.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const mongoDBConnection= ()=>{
-mongoose.connect(process.env.DB_CONNECTION_URL).then(()=>{
-    console.log("mongo server connection.success..");
-}).catch((error)=>{
-    console.log(" error mongo server connection...", error);
-    process.exit(1);
-});
+require('dotenv').config();
+const mongoDBConnection = () => {
+    mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PWD}@cluster1.5r244jl.mongodb.net/`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        family: 4  // 👈 forces IPv4 instead of IPv6
+    }).then(() => {
+        console.log("Server is successfully connected to the Database")
+    }).catch((error) => {
+        console.log("Connection Error Occured!", error);
+        console.log(`Error name is ${error.name} and Error message is ${error.message}`);
+        console.log("=====UNCAUGHT EXCEPTION! 💥 Shutting down=====");
+        process.exit(1);
+    })
 }
-
-module.exports = 
-mongoDBConnection
+module.exports = mongoDBConnection
